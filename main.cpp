967bb1421a5fd8fa14354a8c085adfbc2d31f2b1
sam-payne <sam_payne99@yahoo.co.uk>
2021-06-10 17:22:57 +0100

    Add help option (sim help)

--- main.cpp
+++ main.cpp
@@ -16,36 +16,24 @@ int main (int argc, char* argv[]){
         string in = argv[1];
         if(in == "season")
             RunSeason();
-    } else if (argc == 5){
+        if(in == "help")
+            ShowHelp();
+    } 
+    else if (argc == 5){
         RunGame(argv[1],atoi(argv[2]),argv[3],atoi(argv[4]));
     }
     else if (argc == 3){
         string in = argv[1];
         string team = argv[2];
-        if(in == "howmany"){
+        if(in == "howmany")
             HowManyToWin(team);
-        }
     }
     else{
-        cerr << "Enter either: << \n'sim season' or \n'sim [TEAM] [RANK] [TEAM] [RANK]' or \nsim howmany Burnley";
+        cerr << "Enter either:\n'> sim season' or \n'> sim [TEAM] [RANK] [TEAM] [RANK]' or \n'> sim howmany Burnley' or\n'> sim help'";
         return 1;
     }
     return 0;
 
-    
-    // vector<Team> SortedTeams, FreshTeams, Result;
-    // while(1){
-        
-    //     Result = RunSeason();
-    //     SortedTeams = SortTable(Result);
-        
-    //     if(SortedTeams[0].GetName() == "Burnley")
-    //         break;
-    //     counter++;    
-        
-    // }
-    // cout << counter;
-    
     
 }
 
--- utils.hpp
+++ utils.hpp
@@ -13,11 +13,12 @@ using namespace std;
 
 // Utils.hpp
 void PrintTable(vector<Team> &data);
-vector<Team> SortTable(vector<Team> data);
+vector<Team> SortTable(vector<Team> &data);
 int RunGameOfSeason(Team &home, Team &away);
 int RunSeason(vector<Team> &teams);
 int GetFixtures(int gamenumber,int homeaway);
 int RunGame(Team home,Team away);
+int ShowHelp();
 
 void PrintTable(vector<Team> &data){
     
@@ -73,7 +74,7 @@ void PrintTable(vector<Team> &data){
     }
 }
 
-vector<Team> SortTable(vector<Team> data){
+vector<Team> SortTable(vector<Team> &data){
     sort(data.begin(),data.end());
     reverse(data.begin(),data.end());
     return data;
@@ -559,6 +560,52 @@ int GetFixtures(int gamenumber,int homeaway){
 {12,9},
 {11,10}};
 return fixtures[gamenumber][homeaway];
+}
+
+int ShowHelp(){
+    cout << 
+    "'Footie-sim' by Sam Payne (github.com/sam-payne/footie-sim)\n"
+<< "\n"
+<<"Minute-by-minute football simulator for games and entire seasons\n"
+<<"\n\n"
+<< "Game class:\n"
+<<"A Game object represents a single match, between two 'Side' objects\n"
+<<"\n\n"
+<<"Side class:\n"
+<<"A class to represent a side of 11 player playing at one specific game. Used to track changes goal scoring chance over time. Not to be confused with Team class\n"
+<<"\n\n"
+<<"Team class:\n"
+<<"Hold info about a team's performance over multiple games (season). \n"
+<<"Includes total points, goals for/against etc. Each team has a rank out of 10 (1-10). \n"
+<<"This rank is relative, and used to identify better teams against poorer teams. \n"
+<<"In a league, the best teams shoud be set to 10 and the worst to 1. \n"
+<<"The rank differential between two opposition is used to modify the baseline scoring chance per minute of each team.\n"
+<<"\n\n"
+<<"League:\n"
+<<"Currently the teams in the Premier League 2020/21 season are held in a vector of Teams.\n"
+<<"A pregenerated fixture list is used to simulate a total of 380 games. A league table can then be printed.\n"
+<<"\n\n"
+<<"Usage:\n"
+<<"To compile and link:\n"
+<<"\n"
+<<"> make\n"
+<<"\n"
+<<"To sim a whole season:\n"
+<<"\n"
+<<"> sim season\n"
+<<"\n"
+<<"To sim a game between two teams with relative ranks:\n"
+<<"\n"
+<<"> sim Burnley 10 Chelsea 6\n"
+<<"\n"
+<<"To return the number of seasons simulated before a certain team wins the league:\n"
+<<"\n"
+<<"> sim howmany Burnley\n"
+<<"\n"
+<<"To display help:\n"
+<<"\n"
+<<"> sim help\n\n";
+
 }
 
 #endif
